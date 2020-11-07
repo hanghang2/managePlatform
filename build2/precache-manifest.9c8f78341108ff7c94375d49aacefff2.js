1ac9fe2ffcd83408a912e90c66a2deab77a496e9
zhengyh <zhengyh@xitaiinfo.com>
2020-11-07 15:06:59 +0800

    提交了一个新的演示

---
+++ build2/precache-manifest.9c8f78341108ff7c94375d49aacefff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2392ea96d4647eeb16cd62e69f74de05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13e7651b63e7b69a5592",
+    "url": "./static/css/2.924a9b04.chunk.css"
+  },
+  {
+    "revision": "b11432629c3a1dfd3b50",
+    "url": "./static/css/main.a6a90c49.chunk.css"
+  },
+  {
+    "revision": "13e7651b63e7b69a5592",
+    "url": "./static/js/2.8d4ea2d3.chunk.js"
+  },
+  {
+    "revision": "ca6244337f2a14539cba26e3b67541b5",
+    "url": "./static/js/2.8d4ea2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11432629c3a1dfd3b50",
+    "url": "./static/js/main.31660127.chunk.js"
+  },
+  {
+    "revision": "c632074b5d9b361ec852",
+    "url": "./static/js/runtime-main.9f73449c.js"
+  }
+]);
